ee51c70ee3ef24ec0afd8f4856b27a65a0f131d3
Ali Haider <221081277+qubits-labs@users.noreply.github.com>
2026-03-30 17:30:31 +0500

    reverse-string-added

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,66 @@
-import Image from "next/image";
+"use client";
+import React, { useState } from "react";
 
 export default function Home() {
+  //taking input from user and storing in state
+  const [input, setInput] = useState("");
+  //loading state to show loading spinner
+  const [loading, setLoading] = useState(false);
+  //error state to show error message
+  const [error, setError] = useState("");
+  //result state to show result
+  const [result, setResult] = useState("");
+  //handle submit function to reverse the string
+  const handleSubmit = async (e) => {
+    //prevent default form submission
+    e.preventDefault();
+    //check if input is empty
+    if (!input.trim()) {
+      setError("Please Enter Text");
+      setResult("");
+      return;
+    }
+    //reset error and result state
+    setLoading(true);
+    setError("");
+    //simulate API call with timeout
+    try {
+      await new Promise((res) => setTimeout(res, 500));
+      //reverse the string
+      const reverse = input.split("").reverse().join("");
+      //  update result state with reversed string
+      setResult(reverse);
+      setInput("");
+    } catch (err) {
+      setError("Could not reverse string");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+    <div className="flex flex-col flex-1 items-center justify-center font-sans">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
+        <div>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              placeholder="Please Enter Text"
+              className="border border-gray-300 rounded px-4 py-2 mb-4 w-full placeholder:text-red-500 text-black"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            <button
+              type="submit"
+              onClick={handleSubmit}
+              className="bg-blue-500 text-white py-2 px-4 rounded"
+            >
+              {loading ? "Loading..." : "Click Me"}
+            </button>
+            <p className="mt-4 text-green-600 font-medium">
+              Reverse String {result}
+            </p>
+          </form>
         </div>
       </main>
     </div>
